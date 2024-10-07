73665564135f1d8201e8adc1c57932f42fbb656e
CodeWithMayank-Py <paliwalm4321@gmail.com>
2024-10-07 19:23:20 +0530

    Adding Files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // This should point to your component files
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        warning: "hsl(var(--warning))",
+        "warning-foreground": "hsl(var(--warning-foreground))",
+      },
+    },
   },
   plugins: [],
 }
