a35dad00c39b2493fbc05cc31f5b89c63dfecf01
CodeWithMayank-Py <paliwalm4321@gmail.com>
2024-10-07 22:34:21 +0530

    Adding GoogleAuth

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,3 +22,9 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
+
+// Initialize Firebase Auth
+export const auth = getAuth(app);
+
+// Initialize Google Auth Provider
+export const googleProvider = new GoogleAuthProvider();
