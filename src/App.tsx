6bec134f43e4d44c1ba0a966836157eb6760ef52
CodeWithMayank-Py <paliwalm4321@gmail.com>
2024-10-10 16:44:33 +0530

    Fixing Bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
@@ -8,7 +8,35 @@ import JobApplication from './job-application-tracker';
 import StarryBackground from './landingPage';
 import './index.css';
 
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag: (...args: any[]) => void;
+  }
+}
+
 function App() {
+  useEffect(() => {
+    const GA_MEASUREMENT_ID = process.env.REACT_APP_GA_MEASUREMENT_ID;
+    
+    if (GA_MEASUREMENT_ID) {
+      const script = document.createElement('script');
+      script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+      script.async = true;
+      document.head.appendChild(script);
+
+      window.dataLayer = window.dataLayer || [];
+      window.gtag = function gtag() {
+        window.dataLayer.push(arguments);
+      };
+      
+      window.gtag('js', new Date());
+      window.gtag('config', GA_MEASUREMENT_ID);
+    } else {
+      console.warn('Google Analytics Measurement ID is not set');
+    }
+  }, []);
+
   return (
     <Routes>
       <Route path="/signin" element={<SignIn />} />
