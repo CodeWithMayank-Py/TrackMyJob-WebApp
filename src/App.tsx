0d24bfc1914d562ef989cbae93e2050ff9b40aad
CodeWithMayank-Py <paliwalm4321@gmail.com>
2024-10-10 15:15:36 +0530

    Fixing bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-// src/App.js
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import PrivateRoute from './PrivateRoute';
-import JobApplication from './job-application-tracker';
 import ForgotPassword from './ForgotPassword';
+import JobApplication from './job-application-tracker';
 import StarryBackground from './landingPage';
 import './index.css';
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/job-application-tracker" element={<PrivateRoute><JobApplication /></PrivateRoute>} />
-            <Route path="/" element={<StarryBackground />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/job-application-tracker" element={<PrivateRoute><JobApplication /></PrivateRoute>} />
+      <Route path="/" element={<StarryBackground />} />
+    </Routes>
+  );
 }
 
 export default App;
